c931cb8560a02f2940d8162d060561838302ebc5
Francisco Venegas Rojas <frvenegasrojas@gmail.com>
2019-12-10 12:19:10 -0300

    Cambio extension de RuntimeException

--- src/main/java/cl/globallogic/demo/exception/UsuariosServiceException.java
+++ src/main/java/cl/globallogic/demo/exception/UsuariosServiceException.java
@@ -1,10 +1,11 @@
 package cl.globallogic.demo.exception;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-public class UsuariosServiceException extends  RuntimeException{
+public class UsuariosServiceException extends DataIntegrityViolationException {
     public UsuariosServiceException(String message) {
         super(message);
     }
