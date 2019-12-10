7847b3a2e35c08be202cf7e063c11d426c365d49
Francisco Venegas Rojas <frvenegasrojas@gmail.com>
2019-12-10 01:34:45 -0300

    Cambio handler error

--- src/main/java/cl/globallogic/demo/controller/UsersController.java
+++ src/main/java/cl/globallogic/demo/controller/UsersController.java
@@ -4,16 +4,17 @@ import cl.globallogic.demo.exception.UsuariosServiceErrorAdvice;
 import cl.globallogic.demo.exception.UsuariosServiceException;
 import cl.globallogic.demo.model.Usuarios;
 import cl.globallogic.demo.service.UsuariosService;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
+
 
 import javax.validation.Valid;
-import java.sql.SQLException;
 
 @RestController
 public class UsersController {
--- src/main/java/cl/globallogic/demo/exception/UsuariosServiceErrorAdvice.java
+++ src/main/java/cl/globallogic/demo/exception/UsuariosServiceErrorAdvice.java
@@ -1,6 +1,8 @@
 package cl.globallogic.demo.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -8,22 +10,27 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 import java.sql.SQLException;
 
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+
 
 @ControllerAdvice
+@Slf4j
 public class UsuariosServiceErrorAdvice extends ResponseEntityExceptionHandler {
-    /*@ResponseStatus(HttpStatus.NOT_FOUND)
-    @ExceptionHandler({UsuariosServiceException.class})
-
-    public void handle(UsuariosNotFound e) {}*/
 
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    @ExceptionHandler({UsuariosServiceException.class, SQLException.class, NullPointerException.class})
+    @ExceptionHandler({RuntimeException.class})
+    public ResponseEntity<String> handleRuntimeException(RuntimeException e){
+        return error(INTERNAL_SERVER_ERROR, e);
+    }
 
-    public void handle() {}
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler({UsuariosServiceException.class, SQLException.class})
+    public ResponseEntity<String> handleUsuariosServiceException(UsuariosServiceException e){
+        return error(INTERNAL_SERVER_ERROR, e);
+    }
 
-    /*@ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler({UsuariosServiceException.class})
-
-    public void handle(UsuariosValidationException e) {}*/
-
+    private ResponseEntity<String> error(HttpStatus status, Exception e) {
+        log.info("Exception : ", e);
+        return ResponseEntity.status(status).body(e.getMessage());
+    }
 }
